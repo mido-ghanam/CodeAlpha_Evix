2a3baa4e1f0664b27b506530f6ea5fc828a72b3a
MidoGhanam's Redmi 9T <mghanam883@gmail.com>
2025-12-02 11:16:33 +0200

    02-12-2025 | 11:16:32

--- authentication/urls/web.py
+++ authentication/urls/web.py
@@ -2,7 +2,7 @@ from django.urls import path
 from authentication.views import web as webV
 
 urlpatterns = [
-  path("login/", webV.login, name="login"),
-  path("signup/", webV.registration, name="register"),
+  path("login/", webV.auth.login, name="login"),
+  path("signup/", webV.auth.registration, name="register"),
 
 ]
